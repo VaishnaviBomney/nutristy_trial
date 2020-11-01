d3351f48cef6e07767450677728399c944d3a7d4
Vaishnavi V B <72316149+VaishnaviBomney@users.noreply.github.com>
2020-11-01 12:47:22 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,43 +1,38 @@
 const mongoose = require("mongoose");
 const bcrypt=require("bcrypt");
 const passportLocalMongoose = require("passport-local-mongoose");
-// const UserSchema = new mongoose.Schema({
-//     username:{
-//         type: String,
-//         unique: true,
-//         required: true,
-//         trim: true},
-//     password:{
-//         type: String,
-//         required: true},
-//     email:{
-//         type: String,
-//         unique: true,
-//         required: true,
-//         trim: true},
-//     phone:{
-//         type: Number,
-//         unique: true,
-//         required: true,
-//         }
-// }) ;
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    phone: Number
-});
+     username:{
+         type: String,
+         unique: true,
+         required: true,
+         trim: true},
+     password:{
+         type: String,
+         required: true},
+     email:{
+         type: String,
+         unique: true,
+         required: true,
+         trim: true},
+    phone:{
+         type: Number,
+         unique: true,
+         required: true,
+         }
+ }) ;
 
-// UserSchema.pre('save', function (next) {
-//     var user = this;
-//     bcrypt.hash(user.password, 10, function (err, hash) {
-//       if (err) {
-//         return next(err);
-//       }
-//       user.password = hash;
-//       next();
-//     })
-//   });
+
+ UserSchema.pre('save', function (next) {
+     var user = this;
+     bcrypt.hash(user.password, 10, function (err, hash) {
+       if (err) {
+         return next(err);
+       }
+       user.password = hash;
+       next();
+     })
+   });
 
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User",UserSchema);
